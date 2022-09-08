311f2a9bbe21d31cc589a517a4f416cb7db3a50f
Divye Gupta <divye@Divyes-MacBook-Air.local>
2022-09-08 22:46:33 +0530

    adding contact folder

---
+++ Frontend/src/contact/ContactPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ContactPage.css'
+
+const ContactPage = () => {
+  return (
+    <>
+        <div className='random'>
+            code
+        </div>
+    </>
+    
+  )
+}
+
+export default ContactPage
