58c3c86c41af7c3018fb96ac793c46c8616a84fe
Advait Jain <89680252+Zoronium@users.noreply.github.com>
2022-09-12 17:46:39 +0530

    Refactor: Changed the styled-comp. to native css

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,61 +1,25 @@
 import React from "react";
-import styled from "styled-components";
+import "./Footer.css"
 
 const Footer = () => {
-  const FooterWrapper = styled.div`
-    height: 100px;
-    background: #244d61;
-    height: 200px;
-    display: flex;
-    align-items: stretch;
-    justify-content: space-around;
-    color: white;
-  `;
-
-  const LinksFooter = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    flex-direction: column;
-  `;
-  const Sublink = styled.a`
-    
-    cursor: pointer;
-  `;
-
-  const MediaFooter = styled.div`
-  border: 5px;
-  border-color: black;
-  display: grid;
-  `;
-  const LinksHeader = styled.h3`
-    cursor: pointer;
-  `;
 
   return (
     <div>
-      <FooterWrapper>
-        <MediaFooter>
-            <p>txt</p>
-            <p>txt</p>
-            <p>txt</p>
-        </MediaFooter>
-        <LinksFooter>
-        <LinksHeader>link</LinksHeader>
-          <Sublink>link 1</Sublink>
-          <Sublink>link 1</Sublink>
-          <Sublink>link 1</Sublink>
-          <Sublink>link 1</Sublink>
-        </LinksFooter>
-        <LinksFooter>
-        <LinksHeader>link</LinksHeader>
-          <Sublink>link 1</Sublink>
-          <Sublink>link 1</Sublink>
-          <Sublink>link 1</Sublink>
-          <Sublink>link 1</Sublink>
-          <Sublink>link 1</Sublink>
-        </LinksFooter>
-      </FooterWrapper>
+      <div className="FooterWrapper">
+        <dic className="MediaFooter">
+          <p>txt</p>
+          <p>txt</p>
+          <p>txt</p>
+        </dic>
+        <div className="LinksFooter">
+          <h3 className="LinksHeader">link</h3>
+          <a className="Sublink">link 1</a>
+        </div>
+        <div className="LinksFooter">
+          <h3 className="LinksHeader">link</h3>
+          <a className="Sublink">link 1</a>
+        </div>
+      </div>
     </div>
   );
 };
